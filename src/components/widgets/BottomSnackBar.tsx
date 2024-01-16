e2097f9c46257e2e24b4f12eba707057868b249b
Vishesh Dubey <visheshdubey2016@gmail.com>
2024-01-17 01:42:40 +0530

    BottomSnackBar Fixed

--- src/components/widgets/BottomSnackBar.tsx
+++ src/components/widgets/BottomSnackBar.tsx
@@ -12,14 +12,14 @@ const BottomSnackBar = ({
   return (
     <div
       className={cn(
-        "bg-orange-200  text-xl font-normal items-center px-4 py-4 w-full flex justify-between",
+        "bg-orange-100  gap-4 font-normal items-center px-4 py-4 w-full flex justify-between",
         className
       )}
     >
-      <span className="text-center ">{label}</span>
+      <span className="text-base">{label}</span>
       <a
         href={href}
-        className="px-4 py-2 text-sm text-white rounded-full bg-theme"
+        className="px-4 py-2 text-sm text-white rounded-full w-fit shrink-0 bg-theme"
       >
         Download App
       </a>
